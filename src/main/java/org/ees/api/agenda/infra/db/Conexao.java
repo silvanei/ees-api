bd77db4dff1bcceb999a1c70d82b78ed494e1679
Silvanei Soares Santos <silvanei@dccontrol.com.br>
2016-07-22 09:49:09 -0300

    Melhorias classe de Conexao

--- src/main/java/org/ees/api/agenda/infra/db/Conexao.java
+++ src/main/java/org/ees/api/agenda/infra/db/Conexao.java
@@ -1,4 +1,4 @@
-package org.ees.api.agenda.infra;
+package org.ees.api.agenda.infra.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,41 +7,80 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Conexao {
+
+	private static final String banco = "jdbc:mysql://localhost/ees-agenda?user=root&password=83406310";
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static Connection con = null;
+
+	public static Connection getConexao() {
+		if (con == null) {
+			try {
+				Class.forName(driver);
+				con = DriverManager.getConnection(banco);
+				
+			} catch (ClassNotFoundException ex) {
+				System.out.println("Não encontrou o driver");
+				
+			} catch (SQLException ex) {
+				System.out.println("Erro ao conectar: " + ex.getMessage());
+				
+			}
+		}
+		
+		return con;
+	}
+
+	public static PreparedStatement preparedStatement(String sql) {
+		if (con == null) {
+			con = getConexao();
+		}
+		
+		try {
+			return con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			
+		} catch (SQLException e) {
+			System.out.println("Erro de sql: " + e.getMessage());
+		}
+		
+		return null;
+	}
+
+	public static void beginTransaction() {
+		if (con == null) {
+			con = getConexao();
+		}
+
+		try {
+			con.setAutoCommit(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void commit() {
+		if (con == null) {
+			con = getConexao();
+		}
+
+		try {
+			con.commit();
+			con.setAutoCommit(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	private static final String banco =  "jdbc:mysql://localhost/ees-agenda?user=root&password=83406310";
-    private static final String driver =  "com.mysql.jdbc.Driver";
-    private static Connection con = null;
+	public static void rollback() {
+		if (con == null) {
+			con = getConexao();
+		}
 
-    public static Connection getConexao(){
-        // primeiro testo se o objeto con não foi inicializado
-        if (con == null){
-            try {
-                // defino a classe do driver a ser usado
-                Class.forName(driver);
-                // criação da conexão com o BD
-                con =  DriverManager.getConnection(banco);
-            } catch (ClassNotFoundException ex) {
-                System.out.println("Não encontrou o driver");
-            } catch (SQLException ex) {
-                System.out.println("Erro ao conectar: "+ ex.getMessage());
-            }
-        }
-        return con;
-    }
-
-    public static PreparedStatement getPreparedStatement(String sql){
-        // testo se a conexão já foi criada
-        if (con == null){
-            // cria a conexão
-            con = getConexao();
-        }
-        try {
-            // retorna um objeto java.sql.PreparedStatement
-            return con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        } catch (SQLException e){
-            System.out.println("Erro de sql: "+ e.getMessage());
-        }
-        return null;
-    }
+		try {
+			con.rollback();
+			con.setAutoCommit(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
