851d44d6ec6f3d8d8b28ae04b88fa79d9db4f53f
Silvanei Soares Santos <silvanei@dccontrol.com.br>
2016-07-29 17:25:30 -0300

    Adicionado listAll de serviços do salão

--- src/main/java/org/ees/api/agenda/resource/ServicoResource.java
+++ src/main/java/org/ees/api/agenda/resource/ServicoResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -14,30 +13,25 @@ import org.ees.api.agenda.entity.Servico;
 import org.ees.api.agenda.infra.service.ServicoServiceImpl;
 import org.ees.api.agenda.service.ServicoService;
 
-
 public class ServicoResource {
-	
+
 	private ServicoService servicoService = new ServicoServiceImpl();
 
 	@GET
 	@RolesAllowed("SALAO_ADMIN")
-	@Path("/")
 	public Response listAllServicos(@PathParam("salaoId") Integer idSalao) {
-		
+
 		List<Servico> servicos = servicoService.findByIdSalao(idSalao);
-		
+
 		return Response.status(Status.OK).entity(servicos).build();
 	}
-	
+
 	@POST
 	@RolesAllowed("SALAO_ADMIN")
-	public Response addServico(
-			@PathParam("salaoId") Integer salaoId,
-			Servico servico
-	) {
-		
+	public Response addServico(@PathParam("salaoId") Integer salaoId, Servico servico) {
+
 		Servico newServico = servicoService.insert(salaoId, servico);
-		
+
 		return Response.status(Status.CREATED).entity(newServico).build();
 	}
 }
