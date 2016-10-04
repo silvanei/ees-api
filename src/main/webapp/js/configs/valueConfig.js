9fd9facf40a82755dda80a8feb27094bfb28fe9e
Silvanei Santos <ads.silvanei@gmail.com>
2016-10-04 02:33:04 -0300

    FIX

--- src/main/webapp/js/configs/valueConfig.js
+++ src/main/webapp/js/configs/valueConfig.js
@@ -35,6 +35,7 @@
             calendar: {
                 schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
                 lang: 'pt-BR',
+                timezone: 'local',
                 defaultView: 'agendaDay',
                 slotDuration: '00:30:00', // Intervalo entr as linhas
                 slotLabelFormat: 'H:mm',
